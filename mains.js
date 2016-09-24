0a7a451e35ea74d13e18ca4725c227ba8dcc0df5
五零 <wulin.zwl@taobao.com>
2016-09-24 19:43:42 +0800

    update

---
+++ mains.js
@@ -0,0 +1,7 @@
+
+var counter1 = require('./module/counter');
+var    counter2 = require('./module/counter');
+
+console.log(counter1.count());
+console.log(counter2.count());
+console.log(counter2.count());
---
+++ module/counter.js
@@ -0,0 +1,10 @@
+/**
+ * Created by superstarwulin on 16/4/28.
+ */
+var i = 0;
+
+function count() {
+    return ++i;
+}
+
+exports.count = count;
